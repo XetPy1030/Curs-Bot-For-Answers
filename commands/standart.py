da7d61a8013ed4203378f3bd9558949b9f14f2f1
XetPy1030 <xetpy1030@mail.ru>
2021-12-31 10:09:20 +0500

    Save

--- commands/standart.py
+++ commands/standart.py
@@ -1,16 +1,43 @@
-# command = call Ktoto agas 123
+import commands_other.checklist_types_file
 
+# command = call Ktoto agas 123
 class command_standart:
     manifest = {"name": "base", "desc": "Base class for commands"}
-    checklist_types = {"standart": {"main": ["NoBot", "NoI"]},
-                       }
-    checklist = {"main": []} # {"main": [], "0": [], "1": []}
+    checklist_types = commands_other.checklist_types_file.checklist_types
+    checklist = {"main": {"param": [], "paramEqual": {
+                                                   "author": [],
+                                                   "embeds": [],
+                                                   "channel": [],
+                                                   "reference": [],
+                                                   "mention": [],
+                                                   "channel_mentions": [],
+                                                   "role_mentions": [],
+                                                   "attachments": [],
+                                                   "guild": [],
+                                                   "created_at": []
+                                                   }}} # {"main": [], "0": [], "1": []}
     
     def __init__(self):
         pass
     
     async def check(self, msg, cmd):
-        pass
+        isCor = True
+        if "noBot" in self.checklist["main"]["param"]:
+            isCor = not msg.author.bot
+        if "isBot" in self.checklist["main"]["param"]:
+            isCor = msg.author.bot
+        if "iam" in self.checklist["main"]["param"]:
+            isCor = msg.author.id == self.user.id
+        if "noI" in self.checklist["main"]["param"]:
+            isCor = msg.author.id != self.user.id
+        if "msgReference" in self.checklist["main"]["param"]:
+            isCor = msg.reference != None
+        if "noReference" in self.checklist["main"]["param"]:
+            isCor = msg.reference == None
+        if "doMention" in self.checklist["main"]["param"]:
+            isCor = len(msg.mentions)>0
+        if "noMention" in self.checklist["main"]["param"]:
+            isCor = len(msg.mentions)==0
     
     async def run(self, msg, cmd):#not changed
         isError = await self.check(msg, cmd) #return False if is correct, return Error if.. Yes
---
+++ commands_other/checklist_types_file.py
@@ -0,0 +1,20 @@
+checklist_types = {"standart": {"main": ["noBot", "noI"]},
+                   "all": {"main": {"param": ["noBot", #
+                                              "noI", #
+                                              "msgReference", #
+                                              "doMention" #
+                                              "isBot", #
+                                              "iam", #
+                                              "noReference", #
+                                              "noMention"], #
+                                    "paramEqual": {"author": [], 
+                                                   "roleAuthor": [],
+                                                   "embeds": [],
+                                                   "channel": [],
+                                                   "reference": [],
+                                                   "mention": [],
+                                                   "channel_mentions": [],
+                                                   "role_mentions": [],
+                                                   "attachments": [],
+                                                   "guild": [],
+                                                   "created_at": []}}}}
